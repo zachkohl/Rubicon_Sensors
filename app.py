ac96acfe0aecaa88696c4d6a4ca059806dca0756
sckohl <sckohl@gmail.com>
2018-02-08 06:55:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ bcrypt = Bcrypt(app) #use for encryption
 
 #  DATABASE: use this stuff for Zach's desktop
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kr8tBnnz@localhost:3306/rubiconsensors_0-1'
-=======
+
 #DATABASE: use this stuff for Zach's desktop
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kr8tBnnz@localhost:3306/rubiconsensors_0-1'
 
@@ -49,7 +49,7 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
      password="wf5PWRM4",
      hostname="rubiconsensors.mysql.pythonanywhere-services.com",
      databasename="rubiconsensors$riversensedb",
- )
+)
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
