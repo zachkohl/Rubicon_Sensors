0dfa28e9bfbae89ce9745af0c2f5373c7250e1b2
sckohl <sckohl@gmail.com>
2018-02-20 20:03:59 -0800

    DB02 starting code and scripts included, started to change app.py to reflect connection to DB02

--- app.py
+++ app.py
@@ -59,27 +59,125 @@ db = SQLAlchemy(app)
 
 
 #End database deployment
+#######################################SQLACODEGEN STUFF############################################################
+
+#This is all the sqlacodegen stuff Zach and Sam created.
+#You can use this DB (i.e. the slqacodegen stuff) or the one below it. Just be sure to comment out whichever one you are not using.
+#The sqlacodegen created here is from pip install sqlacodegen, due to the flask-sqlacodegen running into an internal file error. 
+#
+db.Model = declarative_db.Model()
+metadata = db.Model.metadata
+
+
+class Flowsensor(db.Model):
+    __tablename__ = 'flowsensor'
+
+    idFlowSensor = Column(Integer, primary_key=True, nullable=False)
+    Payer_PayerID = Column(ForeignKey('payer.PayerID'), primary_key=True, nullable=False, index=True)
+    Address = Column(String(100))
+
+    payer = db.relationship('Payer')
+    viewer = db.relationship('Viewer', secondary='viewer_has_flowsensor')
+
+
+class Flowsensordata(db.Model):
+    __tablename__ = 'flowsensordata'
+
+    idFlowSensor = Column(Integer, primary_key=True, nullable=False)
+    ISO8601 = Column(String(100))
+    Data = Column(Integer)
+    FlowSensor_idFlowSensor = Column(ForeignKey('flowsensor.idFlowSensor'), primary_key=True, nullable=False, index=True)
+
+    flowsensor = db.relationship('Flowsensor')
+
+
+class Payer(db.Model):
+    __tablename__ = 'payer'
+
+    PayerID = Column(Integer, primary_key=True, unique=True)
+    UserName = Column(String(45))
+    Email = Column(String(90))
+    FirstName = Column(String(45))
+    LastName = Column(String(45))
+    Password = Column(String(100))
+    register_date = Column(String(100))
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    __table_args__ = (
+        ForeignKeyConstraint(['Viewer_idViewer', 'Viewer_Payer_PayerID'], ['viewer.idViewer', 'viewer.Payer_PayerID']),
+        Index('fk_User_Viewer1_idx', 'Viewer_idViewer', 'Viewer_Payer_PayerID')
+    )
+
+    idUser = Column(Integer, primary_key=True, nullable=False)
+    Email = Column(String(45))
+    Name = Column(String(45))
+    FirstName = Column(String(45))
+    LastName = Column(String(45))
+    Address = Column(String(45))
+    Password = Column(String(45))
+    UserName = Column(String(45))
+    Usercol = Column(String(45))
+    Viewer_idViewer = Column(Integer, primary_key=True, nullable=False)
+    Viewer_Payer_PayerID = Column(Integer, primary_key=True, nullable=False)
+    Payer_PayerID = Column(ForeignKey('payer.PayerID'), primary_key=True, nullable=False, index=True)
+
+    payer = db.relationship('Payer')
+    viewer = db.relationship('Viewer')
+
+
+class Viewer(db.Model):
+    __tablename__ = 'viewer'
+
+    idViewer = Column(Integer, primary_key=True, nullable=False)
+    PayerID = Column(Integer)
+    Login = Column(String(45))
+    FirstName = Column(String(45))
+    LastName = Column(String(45))
+    Email = Column(String(90))
+    Payer_PayerID = Column(ForeignKey('payer.PayerID'), primary_key=True, nullable=False, index=True)
+
+    payer = db.relationship('Payer')
+
+
+t_viewer_has_flowsensor = Table(
+    'viewer_has_flowsensor', metadata,
+    Column('Viewer_idViewer', ForeignKey('viewer.idViewer'), primary_key=True, nullable=False, index=True),
+    Column('FlowSensor_idFlowSensor', ForeignKey('flowsensor.idFlowSensor'), primary_key=True, nullable=False, index=True)
+)
+
+
+    def __init__(self, username, email,password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 ########################################END DATABASE STUFF########################################################
 
 
-#Other database models
-    #Create a model of the database for use in python
-class pipe_sensor(db.Model): #The name is the name from the SQL database. This is not about setting up a SQL database!
-                               #It is about creating a local model of the far away SQL database
-                               #We pass in db.model because that will turn the class into something that SQLAlchemy can use SPECIAL TO FLASK SQLALCHEMY
-                               #Recall  db = SQLAlchemy(app)
-    __tablename__ = "pipe_sensor" #The name of the actual SQL table that this local python class is going to represent
-    id = db.Column('id', db.Integer, primary_key=True) #Describes the first column.
-                                                                #Input arguments are the column name, what the datatype is, and if it is a primary key
-                                                                #Don't have to worry about auto imcrement normally because SQL does that automatically. See http://docs.sqlalchemy.org/en/latest/core/metadata.html#sqlalchemy.schema.Column.params.onupdate
 
-    ISO8601 = db.Column('ISO8601', db.String)                   #descriptions of the other columns, for explanation of legal data types, see https://dev.mysql.com/doc/refman/5.7/en/numeric-types.html
-                                                                #Recall also that this is flask-SQLAlchemy, so google the docs for more info.
-    data = db.Column('data', db.Integer)
-    timestamp = db.Column('timestamp', db.String)
+# #Other database models
+#     #Create a model of the database for use in python
+# class pipe_sensor(db.Model): #The name is the name from the SQL database. This is not about setting up a SQL database!
+#                                #It is about creating a local model of the far away SQL database
+#                                #We pass in db.model because that will turn the class into something that SQLAlchemy can use SPECIAL TO FLASK SQLALCHEMY
+#                                #Recall  db = SQLAlchemy(app)
+#     __tablename__ = "pipe_sensor" #The name of the actual SQL table that this local python class is going to represent
+#     id = db.Column('id', db.Integer, primary_key=True) #Describes the first column.
+#                                                                 #Input arguments are the column name, what the datatype is, and if it is a primary key
+#                                                                 #Don't have to worry about auto imcrement normally because SQL does that automatically. See http://docs.sqlalchemy.org/en/latest/core/metadata.html#sqlalchemy.schema.Column.params.onupdate
 
-    #We now have a map for SQLAlchemy to use to relate tot the database. This will let us do all the fun SQLAlchemy commands to electron1
-    # or whatever we name it. Things like pipe_sensor.query.all() See functions for use examples.
+#     ISO8601 = db.Column('ISO8601', db.String)                   #descriptions of the other columns, for explanation of legal data types, see https://dev.mysql.com/doc/refman/5.7/en/numeric-types.html
+#                                                                 #Recall also that this is flask-SQLAlchemy, so google the docs for more info.
+#     data = db.Column('data', db.Integer)
+#     timestamp = db.Column('timestamp', db.String)
+
+#     #We now have a map for SQLAlchemy to use to relate tot the database. This will let us do all the fun SQLAlchemy commands to electron1
+#     # or whatever we name it. Things like pipe_sensor.query.all() See functions for use examples.
 
 ###################################################LOGIN STUFF######################################################
 #See https://blog.pythonanywhere.com/158/
@@ -112,7 +210,7 @@ class User(UserMixin):
     #The __init__ is called aa constructor. We can only have one per class. Page 372 in the book
     #talks about this. This is a special function that will be called as soon as the "User" object
     #is created.
-    #Within the parantheses, the first parameter must be a reference to teh object itself. By convention
+    #Within the parantheses, the first parameter must be a reference to the object itself. By convention
     #this is named "self."
     #Self can now be used inside the class to refer to the object itself. This is just special notation
     #that has to be this way because of the way python is set up.
